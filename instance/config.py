489c9e6d7c1df86f3f0b370f862e3bc2ab0df1f1
Hannah Reiss <hannahcreiss@gmail.com>
2016-04-03 14:18:26 -0400

    db fix 2

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI="postgres://aepbfjdfvgfmqi:JfaSJUyb_gggEoPlujYf5fKUKY@ec2-54-235-93-178.compute-1.amazonaws.com:5432/dbmsr8ock1e5p7"
+SQLALCHEMY_DATABASE_URI="postgres://sperhyipltazqv:4bqAbtCfc9wKIQ99zlk5tcj-mM@ec2-23-21-157-223.compute-1.amazonaws.com:5432/d9btl698kahf16"
 SESSION_TYPE = 'memcached'
