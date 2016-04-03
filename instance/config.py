93db0e38693200a3133d998e565e4022817a3619
Hannah Reiss <hannahcreiss@gmail.com>
2016-04-03 14:15:56 -0400

    db fix

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI="postgresql://hannah:Pioneer1994@localhost/todoapp"
+SQLALCHEMY_DATABASE_URI="postgres://aepbfjdfvgfmqi:JfaSJUyb_gggEoPlujYf5fKUKY@ec2-54-235-93-178.compute-1.amazonaws.com:5432/dbmsr8ock1e5p7"
 SESSION_TYPE = 'memcached'
